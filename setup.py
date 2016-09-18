5bb51ba5b003f59ac5520f38f203d1fc02570f6c
eoinmurray <amonecho1@gmail.com>
2016-09-18 13:48:35 +0100

    0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='rinobot_plugin',
     description='Rinobot plugin helpers',
-    version='0.0.4',
+    version='0.0.5',
 
     author='Eoin Murray',
     author_email='eoin@rinocloud.com',
