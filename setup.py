0196f2a5a01a85a2f4859755b262bf093cd4eb45
eoinmurray <amonecho1@gmail.com>
2016-09-18 15:10:02 +0100

    load tries a few delimiters

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='rinobot_plugin',
     description='Rinobot plugin helpers',
-    version='0.0.5',
+    version='0.0.6',
 
     author='Eoin Murray',
     author_email='eoin@rinocloud.com',
